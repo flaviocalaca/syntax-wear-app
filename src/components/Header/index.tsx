9ea24fdc23af18efe5361e531aad89b429dcb337
Jose Flavio <flaviopereiralc@gmail.com>
2026-07-02 23:11:04 -0300

    feat: adiciona pagina de sobre

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
                 <Link to="/our-stores">Nossas lojas</Link>
               </li>
               <li className="hidden md:block">
-                <a href="#">Sobre</a>
+                <Link to="/about">Sobre</Link>
               </li>
               <li>
                 <Link to="/sign-in">
