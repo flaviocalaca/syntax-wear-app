2bdab442cda465e7acbdf982b70744f3de13a3fc
Jose Flavio <flaviopereiralc@gmail.com>
2026-07-03 18:30:45 -0300

    feat: adiciona menu mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,50 +2,60 @@ import logo from "@/assets/images/Logo.png";
 import IconUser from "@/assets/images/icon-user.png";
 import IconAbolt from "@/assets/images/icon-about.png";
 import { Link } from "@tanstack/react-router";
-import { ShoppingCart } from '../ShoppingCart';
+import { ShoppingCart } from "../ShoppingCart";
+import { MenuMobile } from "../../components/MenuMobile";
+
+export interface NaveLink {
+  name: string;
+  href: string;
+}
+
+const navLinks: NaveLink[] = [
+  { name: "Masculino", href: "/products" },
+  { name: "Feminino", href: "/products" },
+  { name: "Outlet", href: "/products" },
+];
 
 export const Header = () => {
   return (
     <div className="relative">
       <header className="fixed top-5 left-0 right-0 z-10 mx-10 ">
         <div className="bg-white text-black max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
-          <Link to='/'><img className="w-32 md:w-36" src={logo} alt="Logo" /></Link>
+          <Link to="/">
+            <img className="w-32 md:w-36" src={logo} alt="Logo" />
+          </Link>
 
-          <nav className="hidden md:block">
-            <ul className="flex gap-4 md:gap-10">
-              <li>
-                <a href="#">Masculino</a>
-              </li>
-              <li>
-                <a href="#">Feminino</a>
-              </li>
-              <li>
-                <a href="#">Outlet</a>
-              </li>
+          <nav className="hidden lg:block">
+            <ul className="flex gap-10">
+              {navLinks.map((link) => (
+                <Link key={link.name} to={link.href}>
+                  {link.name}
+                </Link>
+              ))}
             </ul>
           </nav>
           <nav>
-            <ul className="flex gap-10">
-              <li className="hidden md:block">
+            <ul className="flex gap-4 md:gap-10 items-center">
+              <li className="hidden lg:block">
                 <Link to="/our-stores">Nossas lojas</Link>
               </li>
-              <li className="hidden md:block">
+              <li className="hidden lg:block">
                 <Link to="/about">Sobre</Link>
               </li>
-              <li>
+              <li className="md:hidden">
+                <MenuMobile navLinks={navLinks} />
+              </li>
+              <li className="hidden lg:block">
                 <Link to="/sign-in">
                   <img src={IconUser} alt="Ícone de login" />
                 </Link>
               </li>
-              <li>
-                <a href="#">
+              <li className="hidden lg:block">
+                <Link to="/about">
                   <img src={IconAbolt} alt="Ícone sobre" />
-                </a>
+                </Link>
               </li>
               <li>
-                {/* <a href="#">
-                  <img src={Iconcart} alt="Ícone do carrinho" />
-                </a> */}
                 <ShoppingCart />
               </li>
             </ul>
---
+++ src/components/MenuMobile/index.tsx
@@ -0,0 +1,64 @@
+import IconMenu from "@/assets/images/icon-menu.png";
+import { Link } from "@tanstack/react-router";
+import { useState } from "react";
+import { FaRegUserCircle } from "react-icons/fa";
+import type { NaveLink } from "../Header";
+import { IoMdClose } from 'react-icons/io';
+
+interface MenuMobileProps {
+  navLinks: NaveLink[];
+}
+
+export const MenuMobile = ({ navLinks }: MenuMobileProps) => {
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <button
+        className="cursor-pointer"
+        onClick={() => setMenuIsOpen(!menuIsOpen)}
+      >
+        <img src={IconMenu} alt="Ícone menu" />
+      </button>
+
+      {/* { Overlay } */}
+      <div
+        className={`${menuIsOpen ? "bg-black/70 visible" : "bg-transparent invisible"} fixed top-0 bottom-0 left-0 right-0 z-30`}
+        onClick={() => setMenuIsOpen(!menuIsOpen)}
+      >
+        {/* { Drawer } */}
+        <div
+          className={`${menuIsOpen ? "translate-x-0" : "-translate-x-full"} absolute top-0 bottom-0 bg-white pt-6 transition-all duration-500 ease-in-out w-full `}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <header className="bg-black py-5 px-5 text-white ">
+            <nav className="flex justify-between">
+              <Link to="/sign-in" className="flex items-center gap-3">
+                <FaRegUserCircle className="h-6 w-6" />
+                <p>Ola! Faça seu login</p>
+              </Link>
+              <IoMdClose className="cursor-pointer text-2xl" onClick={() => setMenuIsOpen(!menuIsOpen)} />
+            </nav>
+          </header>
+          <ul
+            className="p-4 overflow-y-auto scrollbar-hide h-[calc(100% - 140px)] flex flex-col gap-3"
+            onClick={() => setMenuIsOpen(!menuIsOpen)}
+          >
+            {navLinks.map((link) => (
+              <Link key={link.name} to={link.href}>
+                {link.name}
+              </Link>
+            ))}
+
+            <li>
+              <Link to="/our-stores">Nossas lojas</Link>
+            </li>
+            <li>
+              <Link to="/about">Sobre</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
